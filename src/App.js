382581db4245c1035972a2d029e5f3d35e6632a1
jairorjuela <genius85862@gmail.com>
2018-04-01 00:35:52 -0500

    Guest_4

--- src/App.js
+++ src/App.js
@@ -6,78 +6,82 @@ class App extends Component {
     super();
     this.state = {
       newGuest:[],
-      lastName: "",
-      name: ""
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-
-    if(event.target.name === "last-name"){
-      this.setState({
-        lastName: event.target.value
-      });
-    }else{
-      this.setState({
-        name: event.target.value
-      });
-    }
-  }
-
-
-  handleSubmit(event) {
-    let oldTasks = this.state.newGuest
-    let newTask = {
-      lastName: this.state.lastName,
-      name: this.state.name
-    }
-    this.setState({
-      newGuest: [...oldTasks, newTask],
-      newTask: '',
       name: "",
       lastName: ""
+    };
+    this.newName = this.newName.bind(this);
+    this.newLastname = this.newLastname.bind(this);
+    this.createUser = this.createUser.bind(this);
+  }
+
+  newName(event) {
+    this.setState({
+      name: event.target.value
     })
-    event.preventDefault()
+  }
+
+  newLastname(event) {
+    this.setState({
+      lastName: event.target.value
+    })
+  }
+
+
+  createUser(event) {
+    const order = this.state.newGuest.length === 0 ? 1 : this.state.newGuest.length + 1
+
+      let oldTasks = this.state.newGuest
+      let newTask = {
+        id: order,
+        name: this.state.name,
+        lastName: this.state.lastName
+      }
+      this.setState({
+        newGuest: [...oldTasks, newTask],
+        newTask: '',
+        name: "",
+        lastName: ""
+      })
+      event.preventDefault()
+
   }
 
   renderTasks() {
-    const tasks = this.state.newGuest.map(name => {
+    const guest = this.state.newGuest.map(name => {
       return (
-        <tr>
+        <tr key={name.id}>
           <td>{name.name}</td>
           <td>{name.lastName}</td>
         </tr>
       )
     })
-    return tasks
+    return guest
   }
 
   render() {
+
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
 
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={this.createUser}>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" value={this.state.name} onChange={this.handleChange}/>
+                <input type="text" className="form-control" name="first-name" onChange={this.newName}/>
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" value={this.state.lastName} onChange={this.handleChange}/>
+                <input type="text" className="form-control" name="last-name" onChange={this.newLastname}/>
               </div>
 
               <div className="action">
-                <button type="submit" className="btn btn-primary" onClick={this.handleSubmit}>Agregar Invitado</button>
+                <button type="submit" className="btn btn-primary">Agregar Invitado</button>
               </div>
             </form>
 
             <table className="table bordered-table table-striped">
-
               <thead>
                 <tr>
                   <th>Nombre</th>
